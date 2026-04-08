2df0a7f818ffa6651c789ff01fc25cf42fd085e9
Edvardas <edvardas.kozlovas@gmail.com>
2026-04-08 21:27:30 +0100

    feat: add chirp validation, metrics, and bad word filtering

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
+
+var badWords = []string{"kerfuffle", "sharbert", "fornax"}
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
@@ -22,9 +28,10 @@ func main() {
 	config := apiConfig{}
 	mux := http.NewServeMux()
 	mux.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("./app/")))))
-	mux.HandleFunc("/healthz", handlerHealthCheck)
-	mux.HandleFunc("/metrics", config.handerMetrics)
-	mux.HandleFunc("/reset", config.handerReset)
+	mux.HandleFunc("GET /admin/healthz", handlerHealthCheck)
+	mux.HandleFunc("GET /admin/metrics", config.handerMetrics)
+	mux.HandleFunc("POST /admin/reset", config.handerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	server := http.Server{Addr: ":8080", Handler: mux}
 	err := server.ListenAndServe()
@@ -40,9 +47,9 @@ func handlerHealthCheck(w http.ResponseWriter, req *http.Request) {
 }
 
 func (cfg *apiConfig) handerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write(fmt.Append([]byte{}, "Hits: ", cfg.fileserverHits.Load()))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) handerReset(w http.ResponseWriter, req *http.Request) {
@@ -52,4 +59,65 @@ func (cfg *apiConfig) handerReset(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("200 OK"))
+}
+
+func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+
+	defer req.Body.Close()
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		respondWithError(w, 500, "could not read the body")
+		return
+	}
+
+	params := requestBody{}
+	err = json.Unmarshal(data, &params)
+	if err != nil {
+		respondWithError(w, 500, "could not unmarshal the body")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "chirp is longer the 140 chars")
+		return
+	}
+
+	filteredSentence := filterBadWords(params.Body)
+	type responseBody struct {
+		Valid       bool   `json:"valid"`
+		CleanedBody string `json:"cleaned_body,omitempty"`
+	}
+	respondWithJSON(w, 200, responseBody{Valid: true, CleanedBody: filteredSentence})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func filterBadWords(sentence string) string {
+	words := strings.Split(sentence, " ")
+	filtered := make([]string, 0, len(words))
+	for _, word := range words {
+		if slices.Contains(badWords, strings.ToLower(word)) {
+			filtered = append(filtered, "****")
+			continue
+		}
+		filtered = append(filtered, word)
+	}
+	return strings.Join(filtered, " ")
 }
